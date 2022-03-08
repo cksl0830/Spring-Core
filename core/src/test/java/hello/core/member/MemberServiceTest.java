195f496d8a3e462a5d1111d44870b9727b47ed51
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-03-08 16:42:22 +0900

    의존관계 (생성자 주입)
    OCP DIP 성립!

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,19 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberservice = new MemberServiceImpl();
+    MemberService memberservice;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig=new AppConfig();
+        memberservice = appConfig.memberService();
+    }
 
     @Test
     void join() {
