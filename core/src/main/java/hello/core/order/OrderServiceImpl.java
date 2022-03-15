9406d730c1aed8485b09bf9524ebf7a97e695c02
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-03-15 22:13:54 +0900

    @RequiredArgsConstructor
    lombok

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
