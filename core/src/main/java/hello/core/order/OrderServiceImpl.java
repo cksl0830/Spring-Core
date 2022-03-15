8b30dd6d2f1614b13fb2cfd6ef7e33d445988398
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-03-15 22:13:33 +0900

    lombok 추가
    @RequiredArgsConstructor

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,13 +12,15 @@ public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
     // private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); #DIP 위반
     // private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); #변경시 OCP 위반
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//   @RequiredArgsConstructor 가 자동으로 만들어줌
+//    @Autowired (생성자가 하나면 안해줘도댐)
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
